b85e53b2fdcebcc1829ce6a55c26e276beaf0885
Cordones <cordones_91@hotmail.com>
2016-01-22 09:49:49 +0100

    Ejercicio del pato

---
+++ Pato/src/pato/Pato.java
@@ -0,0 +1,26 @@
+package pato;
+
+public class Pato {
+	
+	private int Posicion;
+	
+	public Pato(int pos)
+	{
+		this.Posicion=pos;
+	}
+
+	public int AndarHaciaAdelante(int pasos)
+	{
+		return this.Posicion+=pasos;
+	}
+	
+	public int AndarHaciaAtras(int pasos)
+	{
+		return this.Posicion-=pasos;
+	}
+
+	public int DimePosicion()
+	{
+		return this.Posicion;
+	}
+}
---
+++ Pato/src/pato/Principal.java
@@ -0,0 +1,23 @@
+package pato;
+
+import java.util.Scanner;
+
+public class Principal {
+	
+	static Scanner lector=new Scanner(System.in);
+	
+	public static void main(String[] args) {
+		// TODO Apéndice de método generado automáticamente
+	
+		//int pasos;
+		Pato pepito=new Pato(5);
+		
+		pepito.AndarHaciaAdelante(22);
+		
+		System.out.println("La posicion del pato es: "+pepito.DimePosicion());
+		
+	
+
+	}
+
+}
